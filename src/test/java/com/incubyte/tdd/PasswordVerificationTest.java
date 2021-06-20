6e2654657475ca5a6ba3a7dc7a3aaca3b870b54f
chanchal-detani <chanchaldetani@gmail.com>
2021-06-20 23:47:24 +0530

    Added negative test case for password length.

--- src/test/java/com/incubyte/tdd/PasswordVerificationTest.java
+++ src/test/java/com/incubyte/tdd/PasswordVerificationTest.java
@@ -1,18 +1,34 @@
 package com.incubyte.tdd;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import com.incubyte.tdd.domain.Password;
 
 public class PasswordVerificationTest {
 
-    @Test public void
-    test_password_length() {
+    @Test
+    public void test_password_length() {
         // positive test case
         // password should be of 8 characters long
         new Password("password").validate();
         assertTrue(true);
+    }
+
+    @Test
+    public void should_throw_required_exception_for_minimum_length() {
+        
+        // checking minimum length of the password
+        Throwable exception = assertThrows(VerificationFailedException.class, () -> {
+            new Password("pwd").validate();
+        });
+
+        assertEquals("Password should be at least 8 characters long.", exception.getMessage());
+
     }
 }
